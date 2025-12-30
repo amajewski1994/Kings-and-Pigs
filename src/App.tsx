c237660240ac6ab8029d29a7062898f9a23e5367
Andrevfcb <amajewski1994@gmail.com>
2025-12-30 13:43:57 +0100

    add PixiJS integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { Application } from "@pixi/react";
+import { Texture } from "pixi.js";
+
 function App() {
   return (
-    <>
-      Hello world
-    </>
+    <Application width={800} height={600} backgroundColor={0x1099bb}>
+      <pixiSprite
+        texture={Texture.WHITE}
+        x={100}
+        y={100}
+        width={100}
+        height={100}
+        tint={0xff0000}
+      />
+    </Application>
   )
 }
 
